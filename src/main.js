60b26fa5b9de6d352cc25b8b254cce101ccb1113
Mingy1024 <s2142717@gmail.com>
2022-03-26 01:57:05 +0800

    建立訂單畫面

--- src/main.js
+++ src/main.js
@@ -6,8 +6,14 @@ import App from './App.vue'
 import router from './router'
 import Loading from 'vue-loading-overlay'
 import 'vue-loading-overlay/dist/vue-loading.css'
+import { date, currency } from './methods/filters'
 
 const app = createApp(App)
+app.config.globalProperties.$filters = {
+  date,
+  currency
+}
+
 app.use(router)
 app.use(VueAxios, axios)
 app.component('VLoading', Loading)
--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -1,3 +1,101 @@
 <template>
-    <h2>訂單</h2>
+  <VLoading :active="isLoading" :z-index="1060"></VLoading>
+  <table class="table mt-4">
+    <thead>
+      <tr>
+        <th>購買時間</th>
+        <th>Email</th>
+        <th>購買款項</th>
+        <th>應付金額</th>
+        <th>是否付款</th>
+        <th>編輯</th>
+      </tr>
+    </thead>
+    <tbody>
+      <template v-for="(item, key) in orders" :key="key">
+        <tr v-if="orders.length" :class="{ 'text-secondary': !item.is_paid }">
+          <td>{{ $filters.date(item.create_at) }}</td>
+          <td><span v-text="item.user.email" v-if="item.user"></span></td>
+          <td>
+            <ul class="list-unstyled">
+              <li v-for="(product, i) in item.products" :key="i">
+                {{ product.product.title }} 數量：{{ product.qty }}
+                {{ product.product.unit }}
+              </li>
+            </ul>
+          </td>
+          <td class="text-right">{{ item.total }}</td>
+          <td>
+            <div class="form-check form-switch">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                :id="`paidSwitch${item.id}`"
+                v-model="item.is_paid"
+                @change="updatePaid(item)"
+              />
+              <label class="form-check-label" :for="`paidSwitch${item.id}`">
+                <span v-if="item.is_paid">已付款</span>
+                <span v-else>未付款</span>
+              </label>
+            </div>
+          </td>
+          <td>
+            <div class="btn-group">
+              <button
+                class="btn btn-outline-primary btn-sm"
+                type="button"
+                @click="openModal(item)"
+              >
+                檢視
+              </button>
+              <button
+                class="btn btn-outline-danger btn-sm"
+                type="button"
+                @click="openDelOrderModal(item)"
+              >
+                刪除
+              </button>
+            </div>
+          </td>
+        </tr>
+      </template>
+    </tbody>
+  </table>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      orders: {},
+      tempOrder: {},
+      isLoading: false,
+      pagination: {},
+      currentPage: 1
+    }
+  },
+  methods: {
+    getOrders (currentPage = 1) {
+      this.currentPage = currentPage
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/orders?page=${currentPage}`
+      this.isLoading = true
+      this.$http
+        .get(api)
+        .then((res) => {
+          console.log(res)
+          this.orders = res.data.orders
+          this.pagination = res.data.pagination
+          this.isLoading = false
+        })
+        .catch((err) => {
+          this.isLoading = false
+          console.log(err)
+        })
+    }
+  },
+  mounted () {
+    this.getOrders()
+  }
+}
+</script>
